e696d74be4e71dd564eccb92f44515378f254307
Bruno Borelli <brunoborellipro@gmail.com>
2021-01-23 13:07:22 -0300

    Testes com emojis

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,43 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useState, useEffect} from "react"
+import axios from "axios"
+import Emoji from "react-emoji-render"
 
 export default function Home() {
+  const [nome, setNome] = useState()
+  const[emoji, setEmoji] = useState(":heart:")
+  const responseData = async function(){
+   const res = await axios.get("http://localhost:3000/api/hello")
+   setNome(res.data.name)
+  }
+  const trocaEmoji = function(){
+    const i = Math.floor(Math.random()*listaEmojis.length)
+    console.log(i)
+    setEmoji(listaEmojis[i])
+  }
+
+  const listaEmojis = [
+    ":heart:",
+    ":heart_eyes_cat:",
+    ":purple_heart:",
+    ":sparkles:",
+    ":heart_eyes:",
+    ":kissing_heart:",
+    ":rainbow:"
+  ]
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>
+        Oi Jamille!
+      </h1>
+      <h2>
+        Tudo bom?
+      </h2>
+      <Emoji className="emoji" text={emoji} onlyEmojiClassName="make-emojis-large" />
+      <h3>Meu nome é {responseData(), nome}</h3>
+      <button onClick={trocaEmoji}>Clique aqui</button>
     </div>
   )
 }
